877adb1ac3e3da9455cfe4e1689f8c22c7f3f1ca
Mike Solomon <mike@meeshkan.com>
2021-06-05 16:05:28 +0300

    Adds env

--- example-1/packages.dhall
+++ example-1/packages.dhall
@@ -177,7 +177,7 @@ let additions =
           , "typelevel-prelude"
           ]
         , repo = "https://github.com/mikesol/purescript-wags.git"
-        , version = "v0.2.3"
+        , version = "v0.2.4"
         }
       , painting =
         { dependencies =
--- example-1/src/Audio.purs
+++ example-1/src/Audio.purs
@@ -2,20 +2,27 @@ module Audio where
 
 import Prelude
 
-import Control.Applicative.Indexed (ivoid)
 import Control.Apply.Indexed ((:*>))
+import Control.Comonad.Cofree (head, tail)
+import Data.Int (toNumber)
+import Data.Lens (_1, over, traversed)
+import Data.List (List(..), (..), (:))
+import Data.NonEmpty (NonEmpty, (:|))
 import Data.Tuple.Nested (type (/\), (/\))
 import Data.Typelevel.Num (D4, D5)
 import Data.Vec ((+>))
 import Data.Vec as V
 import Effect (Effect)
 import WAGS.Change (ichange)
-import WAGS.Control.Functions.Validated (freeze, (@!>))
+import WAGS.Control.Functions.Validated (iloop, (@!>))
 import WAGS.Control.Indexed (IxWAG)
 import WAGS.Control.Types (Frame0, Scene)
 import WAGS.Graph.AudioUnit (TGain, TPeriodicOsc, TSpeaker)
 import WAGS.Graph.Optionals (gain_, periodicOsc_)
+import WAGS.Graph.Parameter (ff)
 import WAGS.Interpret (FFIAudio)
+import WAGS.Math (calcSlope, calcSlopeExp)
+import WAGS.NE2CF (ASDR, makePiecewise)
 import WAGS.Patch (ipatch)
 import WAGS.Run (SceneI)
 
@@ -45,22 +52,55 @@ type SceneType
 type FrameTp p i o a
   = IxWAG FFIAudio (Effect Unit) p Unit i o a
 
-createFrame :: FrameTp Frame0 {} SceneType Unit
+jnel :: forall a. NonEmpty List (NonEmpty List a) -> NonEmpty List a
+jnel (a :| Nil) = a
+
+jnel ((a :| b) :| ((c :| d) : e)) = jnel ((a :| (b <> pure c <> d)) :| e)
+
+pwf' :: NonEmpty List (Number /\ Number)
+pwf' = (0.0 /\ 0.0) :| (0.03 /\ 1.0) : (0.07 /\ 0.1) : (0.09 /\ 0.0) : Nil
+
+pwf :: NonEmpty List (Number /\ Number)
+pwf = jnel $ map (\i -> over (traversed <<< _1) (add (0.11 * toNumber i)) pwf') (0 :| (1 .. 100))
+
+createFrame :: FrameTp Frame0 {} SceneType { asdr :: ASDR }
 createFrame =
   ipatch
-    :*> ( ivoid
-          $ ichange
-              { mix: gain_ 0.1
-              , unit0: gain_ 0.1
-              , unit1: gain_ 0.1
-              , unit2: gain_ 0.1
-              , osc0: periodicOsc_ osc0 220.0
-              , osc1: periodicOsc_ osc1 440.0
-              , osc2: periodicOsc_ osc2 880.0
-              }
+    :*> ( ichange
+          { mix: gain_ 0.1
+          , unit0: gain_ 0.0
+          , unit1: gain_ 0.0
+          , unit2: gain_ 0.0
+          , osc0: periodicOsc_ osc0 220.0
+          , osc1: periodicOsc_ osc1 440.0
+          , osc2: periodicOsc_ osc2 880.0
+          }
+          $> { asdr: makePiecewise pwf }
       )
 
 piece :: Scene (SceneI Unit Unit) FFIAudio (Effect Unit) Frame0 Unit
 piece =
   (const createFrame)
-    @!> freeze
+    @!> iloop \e { asdr } ->
+        let
+          { time, headroom } = e
+
+          pulse = asdr { time, headroom: toNumber headroom / 1000.0 }
+
+          g' = ff 0.04 $ head pulse
+
+          fade
+            | time < 1.0 = g' $> calcSlope 0.0 0.0 1.0 1.0 time
+            | otherwise = g' $> 1.0
+          
+          g = g' * fade
+
+          ramp = g $> calcSlopeExp 0.0 1.0 10.0 0.0 4.0 time
+
+        in
+          ichange
+            { unit0: gain_ (g * ramp )
+            , unit1: gain_ g
+            , unit2: gain_ (g * (sub 1.0 <$> ramp))
+            }
+            $> { asdr: tail pulse }
