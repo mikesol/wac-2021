a0d2d147c05d2cc260a173755eec4b512cc37dbd
Mike Solomon <mike@meeshkan.com>
2021-07-06 07:13:02 +0300

    Adds intro to frp

---
+++ intro-to-frp/src/Main.purs
@@ -0,0 +1,78 @@
+module Main where
+
+import Prelude
+
+import Data.Maybe (Maybe(..))
+import Effect (Effect)
+import Effect.Aff.Class (class MonadAff)
+import Effect.Class (class MonadEffect)
+import Halogen (ClassName(..))
+import Halogen as H
+import Halogen.Aff (awaitBody, runHalogenAff)
+import Halogen.HTML as HH
+import Halogen.Subscription as HS
+import Halogen.HTML.Properties as HP
+import Halogen.VDom.Driver (runUI)
+
+main :: Effect Unit
+main =
+  runHalogenAff do
+    body <- awaitBody
+    runUI component unit body
+
+type State
+  = { myText :: String
+    }
+
+data Action
+  = Initialize | UpdateUI String
+
+component :: forall query input output m. MonadEffect m => MonadAff m => H.Component query input output m
+component =
+  H.mkComponent
+    { initialState
+    , render
+    , eval: H.mkEval $ H.defaultEval { handleAction = handleAction, initialize = Just Initialize }
+    }
+
+initialState :: forall input. input -> State
+initialState _ =
+  { myText: mempty
+  }
+
+classes :: forall r p. Array String -> HP.IProp ( class :: String | r ) p
+classes = HP.classes <<< map ClassName
+
+render :: forall m. State -> H.ComponentHTML Action () m
+render { myText } =
+  HH.div [ classes [ "w-screen", "h-screen" ] ]
+    [ HH.div [ classes [ "flex", "flex-col", "w-full", "h-full" ] ]
+        [ HH.div [ classes [ "flex-grow" ] ] []
+        , HH.div [ classes [ "flex-grow-0", "flex", "flex-row" ] ]
+            [ HH.div [ classes [ "flex-grow" ] ] []
+            , HH.div_
+                [ HH.h1 [ classes [ "text-center", "text-3xl", "font-bold" ] ]
+                    [ HH.text "Intro to FRP" ]
+                ]
+            , HH.div [ classes [ "flex-grow" ] ] []
+            ]
+        , HH.div [ classes [ "flex-grow-0", "flex", "flex-row" ] ]
+            [ HH.div [ classes [ "flex-grow" ] ] []
+            , HH.div_
+                [ HH.text myText
+                ]
+            , HH.div [ classes [ "flex-grow" ] ] []
+            ]
+        , HH.div [ classes [ "flex-grow" ] ] []
+        ]
+    ]
+
+
+handleAction :: forall output m. MonadEffect m => MonadAff m => Action -> H.HalogenM State Action () output m Unit
+handleAction = case _ of
+  Initialize -> do
+    { emitter, listener } <- H.liftEffect HS.create
+    void $ H.subscribe emitter
+    H.liftEffect $ HS.notify listener (UpdateUI "hello")
+  UpdateUI myText -> do
+    H.modify_ _ { myText = myText }
